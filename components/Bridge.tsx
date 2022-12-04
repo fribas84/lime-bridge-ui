bf9d8385745ffc525b072cc6d2db297b868a0333
fribas <fribas@asapp.com>
2022-12-04 19:39:10 -0300

    read balance of tokens

--- components/Bridge.tsx
+++ components/Bridge.tsx
@@ -3,51 +3,126 @@ import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
 import useToken from "../hooks/useToken";
 import Table from "react-bootstrap/Table";
-import { Button, Dropdown } from "react-bootstrap";
+import { Button, Container, Dropdown, DropdownButton, Form, InputGroup, Row} from "react-bootstrap";
 import Borrowed from "./Borrowed";
 import NewBook from "./NewBook";
 import { ethers } from "ethers";
 import ErrorHandler from "./ErrorHandler";
 import LoaderTransaction from "./LoaderTransaction";
+import useTokenBalance from "../hooks/useTokenBalance";
 
-const Balance = () => {
-  useEffect(() => {}, []);
+type BridgeContract = {
+  GOERLI_TOKEN: string;
+  MUMBAI_TOKEN: string;
+  GOERLI_BRIDGE: string;
+  MUMBAI_BRIDGE: string;
+}
+
+
+
+const Bridge = ({GOERLI_TOKEN,MUMBAI_TOKEN,GOERLI_BRIDGE,MUMBAI_BRIDGE}: BridgeContract) => {
+  const { account, library } = useWeb3React<Web3Provider>();
+  const useGoerliToken = useToken(GOERLI_TOKEN);
+  const useMumbaiToken = useToken(MUMBAI_TOKEN);
+ 
+  const [destNetwork, setDestNetwork] = useState<string>("Mumbai");
+  const [availableTokens, setAvailableTokens] = useState<number>();
+  const [tokensToTx, setTokensToTx] = useState<number>(0);
+  const { data } = useTokenBalance(account, GOERLI_TOKEN);
+  useEffect( () => {
+    const fetchData = async () => {
+      await handleGetAvailableTokens();
+    }
+    fetchData();
+  }, []);
+  
+  const handleMax = () => {
+    setTokensToTx(availableTokens);
+  }
+
+  const handleSelectNetwork = (e) =>{
+    handleGetAvailableTokens();
+    setTokensToTx(0);
+    setDestNetwork(e);
+  }
+
+  const handleGetAvailableTokens = async () =>{
+    if(destNetwork == "Mumbai"){
+      const bal = await useGoerliToken.balanceOf(account);
+      setAvailableTokens(Number(ethers.utils.formatEther(bal)));
+    }
+    if(destNetwork == "Goerli"){
+      const bal = await useMumbaiToken.balanceOf(account);
+      setAvailableTokens(Number(ethers.utils.formatEther(bal)));
+    }
+  } 
+  
+  const handleChangeTokensToTx = (e) =>{
+  setTokensToTx(e.target.value);
+  }
+
+  const handleClear = () =>{
+    setDestNetwork("Mumbai");
+    handleGetAvailableTokens();
+    setTokensToTx(0);
+  }
 
   return (
     <div>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Select Origin Network
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Goerli</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Mumbai</Dropdown.Item>
-      </Dropdown.Menu>
-      </Dropdown>
+      <Container className="justify-content-md-center md">
+      <Table>
+        <tbody>
+        <tr>
+          <td>
+          <span><strong>Selected Network: </strong>{destNetwork}</span>
+          </td>
+          <td>
+            <DropdownButton
+              title="Change Network destination"
+              onSelect={handleSelectNetwork}
+            >
+              <Dropdown.Item eventKey="Goerli">Goerli</Dropdown.Item>
+              <Dropdown.Item eventKey="Mumbai">Mumbai</Dropdown.Item>
+            </DropdownButton>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Available tokens
+          </td>
+          <td>LMT {availableTokens}</td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+          <InputGroup className="mb-3">
+          <InputGroup.Text>LMTs to Transfer:</InputGroup.Text>
+          
+          <Form.Control type="number" min={0} max={availableTokens} aria-label="Tokens to Transfer" value={tokensToTx} onChange={handleChangeTokensToTx}/>
+          <Button variant="outline-secondary" id="button-addon2" onClick={handleMax}>
+          Max
+        </Button>
+          
+        </InputGroup>
+          </td>
+        </tr>
+    
+        </tbody>
+        
+      </Table>
+        <Button variant="danger"  onClick={handleClear}> Clear</Button>{" "}
+        <Button variant="success"> Transfer</Button>
+      </Container>
 
       <style jsx>{`
-        .results-form {
-          display: flex;
-          flex-direction: column;
-        }
-
-        .button-wrapper {
-          margin: 5px;
-          float: right;
-        }
-        .bookRentWrapper {
-          float: left;
-        }
-        .upperTable {
-          position: inline;
-        }
+        
         .actionButton {
-          margin: 1px;
-          padding: 1px;
+          padding: 10px;
+          float:left;
         }
+      
       `}</style>
     </div>
   );
 };
 
-export default Balance;
+export default Bridge;
