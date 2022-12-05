41f11637041d90adba00d2320e3a96a6200b57eb
fribas <fribas@asapp.com>
2022-12-04 21:30:57 -0300

    removed unused files

--- hooks/useLibraryContract.ts
+++
@@ -1,7 +0,0 @@
-import LIBRARY_ABI from "../contracts/LibraryV2.json";
-import type { LibraryV2 } from "../contracts/types";
-import useContract from "./useContract";
-
-export default function useLibraryContract(contractAddress?: string) {
-  return useContract<LibraryV2>(contractAddress, LIBRARY_ABI);
-}
